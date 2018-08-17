98c8b23311e2122baf09762bff6af695d0d88420
James Taylor <james@jamestaylor.org>
2018-08-17 17:35:29 -0400

    Update install_conda_packages.sh

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -2,6 +2,6 @@ conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
 
-conda install bwa bowtie bowtie2 hisat2 stringtie cufflinks samtools bedtools fastqc macs2 \
+conda install bwa bowtie bowtie2 hisat2 stringtie cufflinks samtools bedtools fastqc \
     sra-tools blast \
     numpy scipy pandas sympy pymc scikit-learn scikit-image cython ipython jupyter patsy statsmodels matplotlib seaborn
